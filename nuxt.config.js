5bdd22d1254c21cd90272347dc905cb2eb2c57e0
Vincent Hardouin <vincenthardouin@icloud.com>
2021-05-19 20:46:06 +0200

    feat: Add scss style

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/scss/shared.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -35,6 +35,10 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxt/content', '@nuxtjs/style-resources'],
+
+  styleResources: {
+    scss: ['assets/scss/globals.scss'],
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
