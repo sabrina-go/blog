640dd0b044d79086261892437093361f246aff7c
Vincent Hardouin <vincenthardouin@icloud.com>
2021-05-19 20:43:46 +0200

    build: Install @nuxtjs/style-resources

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxtjs/style-resources'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
