2b80aaaad94a7b7a870cecf668295485c84b7bc3
VincentHardouin <VincentHardouin@users.noreply.github.com>
2021-05-20 19:18:09 +0000

    deploy: 17c96be27b45be45b90c0cb8553b192b71160182

--- _nuxt/static/1621450217/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621450217",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"27d47793"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1621538273/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621538273",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"85041e37"}}}}(null,"\u002F"));
