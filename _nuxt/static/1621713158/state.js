2afd26ffd0b89e65e156e5acbeb60818d5fedc87
sabrina-go <sabrina-go@users.noreply.github.com>
2021-05-22 19:52:55 +0000

    deploy: bf085b07a53434955c7ddba3a9989759ca7b1367

---
+++ _nuxt/static/1621713158/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621713158",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8fe67e0b"}}}}(null,"\u002F"));
