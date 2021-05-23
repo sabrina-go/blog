284e64c6cbc60299e077b584779d055f9bdda727
VincentHardouin <VincentHardouin@users.noreply.github.com>
2021-05-23 19:56:36 +0000

    deploy: 3bcf3d59a7b4bed4627b1fea889702249419aaeb

---
+++ _nuxt/static/1621799781/blog/my-first-blog-post/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1621799781",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fmy-first-blog-post",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"10ef5d12"}}}}(null));
