c5e3d48d78288e18cb2224e5e4d1b568001ccd48
VincentHardouin <VincentHardouin@users.noreply.github.com>
2021-09-13 10:33:18 +0000

    deploy: 498dfdc428dd1d9a8daa1d3c6b43a0e4cf4c8961

---
+++ _nuxt/static/1631529180/blog/introduire-l-agilite-en-douceur/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631529180",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fintroduire-l-agilite-en-douceur",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
