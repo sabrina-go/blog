4c2517fe1cf37ace8de5950dbbe5b47eff486eba
VincentHardouin <VincentHardouin@users.noreply.github.com>
2022-01-17 07:35:25 +0000

    deploy: 6351a85fb7e609b73ebf65ac9c31cd41d4c256b3

--- _nuxt/static/1642175635/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fpreview","\u002F","\u002Fa-propos","\u002Fblog\u002Fagilite-teletravail-top-ou-flop","\u002Fblog\u002Fintroduire-l-agilite-en-douceur","\u002Fblog\u002Fsprint-review","\u002Fblog\u002Fsprint-planning"]})
